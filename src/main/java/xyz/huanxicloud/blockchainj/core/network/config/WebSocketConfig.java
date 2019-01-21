fd7bacdc82410ea20a5dc0b35dcb7fda09f5b1b2
huanxi <1355473748@qq.com>
2019-01-21 21:15:44 +0800

    websocket 服务端

---
+++ src/main/java/xyz/huanxicloud/blockchainj/core/network/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package xyz.huanxicloud.blockchainj.core.network.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author: huanxi
+ * @date: 2019/1/21 20:53
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
